7113aeea98a669ed86a77acad44bfd53263798b4
Doug <doug.cranston@gmail.com>
2020-09-28 11:02:50 -0400

    added app.secret_key to enable sessions

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ config = configparser.ConfigParser()
 config_file = config.read("config.ini")[0]
 
 app = Flask(__name__)
+app.secret_key = "bn94127hf921hf9312h932qe9ufhqe90fh028r18923"
 db_string = "mysql+pymysql://{}:{}@{}/{}".format(
     config["mysql"]["user"],
     config["mysql"]["password"],
